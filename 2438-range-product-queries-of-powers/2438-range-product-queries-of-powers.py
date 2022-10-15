f79cd3554e22a30e556a23de36aff6841d930e1c
Eric Gitangu <developer.ericgitangu@gmail.com>
2022-10-15 17:58:40 -0400

    Time: 4591 ms (20.00%), Space: 51.8 MB (100.00%) - LeetHub

--- 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.py
+++ 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.py
@@ -1,11 +1,6 @@
 class Solution:
     def productQueries(self, n: int, queries: List[List[int]]) -> List[int]:
-        x = bin(n)[2:][::-1]
-        powers = []
-        for i, b in enumerate(x):
-            val = int(b)
-            if val:
-                powers.append(val*(2**i))
+        powers = [int(b)*2**i for i, b in enumerate(bin(n)[2:][::-1]) if int(b)]
         ans = [math.prod(powers[l:r+1])%((10**9) +7) for l, r in queries]
         return ans
             
