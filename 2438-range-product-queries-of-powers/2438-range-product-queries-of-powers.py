4801ecf110ccd2a65bc9a781b350960f242183c2
Eric Gitangu <developer.ericgitangu@gmail.com>
2022-10-15 17:22:38 -0400

    Time: 3380 ms (60.00%), Space: 51.8 MB (100.00%) - LeetHub

---
+++ 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.py
@@ -0,0 +1,18 @@
+class Solution:
+    def productQueries(self, n: int, queries: List[List[int]]) -> List[int]:
+        x = reversed(bin(n)[2:])
+        powers = []
+        for i, b in enumerate(x):
+            val = int(b)
+            if val:
+                powers.append(val*(2**i))
+        ans = []
+        for query in queries:
+            power = 1
+            for i in range(query[0], query[1]+1):
+                power *= powers[i]
+            ans.append(power%((10**9) +7))   
+        return ans
+            
+            
+        
